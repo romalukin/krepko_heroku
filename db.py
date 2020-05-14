f71ee660a215b212e1fca18cc6de72e018e4e2c5
romalukin <romalukin@gmail.com>
2020-05-14 19:11:00 +0300

    commit4

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ Base = declarative_base()
 metadata = MetaData()
 
 class Products(Base):
-    tablename='products'
+    __tablename__='products'
     id = Column(Integer, primary_key=True)
     name = Column( String(100) )
     old_price = Column( Integer )
